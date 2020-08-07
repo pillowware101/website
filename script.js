e95b8b3b0851181184f863d2ca4d8995e222e222
hi101000 <60079731+hi101000@users.noreply.github.com>
2020-08-07 16:08:19 -0400

    added more cookies

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function writeCookie(username){
-	var username2='username'+username;
+function writeCookieusrname(username){
+	var username2='username='+username+'; expires=Thu, 1 Jan 4040 12:00:00 UTC';
 	document.cookie=username2;
+}
+function writeCookiepsswrd(psswrd){
+	var psswrdcookie='password='+psswrd+'; expires=Thu, 1 Jan 4040 12:00:00 UTC';
+	document.cookie=psswrdcookie;
 }
